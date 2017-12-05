7efb3c7f7c8239812104988ad0a8f7ba55124c93
gfrejek <gfrejek@gmail.com>
2017-12-05 18:51:25 +0100

    Made Parser static

--- src/Parser.java
+++ src/Parser.java
@@ -4,16 +4,15 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Parser{
-    private Scanner scanner;
-    private String filePath;
-    private StatuteType fileType;
+    private static Scanner scanner;
+    private static StatuteType fileType;
 
 
     //-----------------------------------------------------------------\\
     // openFile returns null if file with given path cannot be opened  \\
     // openFile returns Scanner if file exists and can be opened       \\
     //-----------------------------------------------------------------\\
-    private Scanner openFile(String path){
+    private static Scanner openFile(String path){
         Scanner result = null;
         FileInputStream inputStream = null;
 
@@ -31,11 +30,11 @@ public class Parser{
     }
 
 
-    //-------------------------------------------------------------------------------\\
-    // parse checks whether or not one of the arguments is path to the desired file  \\
-    // and assigns this path to 'filePath' and assigns Scanner to 'scanner' variable \\
-    //-------------------------------------------------------------------------------\\
-    public Parser(String[] arguments){
+    //-------------------------------------------------------------------------------------\\
+    // Constructor checks whether or not one of the arguments is path to the desired file  \\
+    // and assigns this path to 'filePath' and assigns Scanner to 'scanner' variable       \\
+    //-------------------------------------------------------------------------------------\\
+    public static void parse(String[] arguments){
         boolean foundRightFile = false;
         Scanner tmpScanner;
         StatuteType tmpType = null;
@@ -52,13 +51,29 @@ public class Parser{
                 }
             }
         }
+        String newPath;
+        Scanner s1;
 
-        if(foundRightFile){
-            System.out.println("Statute type: " + tmpType.toString());     //todo Delete
-        } else {
-            System.out.println("Need to specify file path to the right file");
-            // todo Demand an input
+        while(!foundRightFile){
+            System.out.println("Specify file path to the right file (act or constitution)");
+            System.out.print("Path: ");
+            s1 = new Scanner(System.in);
+            newPath = s1.next();
+
+
+            tmpScanner = openFile(newPath);
+            if(tmpScanner != null){
+                tmpType = checkType(tmpScanner);
+                if(tmpType != null){
+                    foundRightFile = true;
+                    fileType = tmpType;
+                    scanner = tmpScanner;
+                    break;
+                }
+            }
         }
+
+        System.out.println("Statute type: " + tmpType.toString() + System.lineSeparator() + "File has been loaded successfully" + System.lineSeparator());
     }
 
 
@@ -66,7 +81,7 @@ public class Parser{
     // checkType returns the type of the file with 'file' handle variable \\
     // checkType returns null if it is not the desirable file             \\
     //--------------------------------------------------------------------\\
-    private StatuteType checkType(Scanner file){
+    private static StatuteType checkType(Scanner file){
         for(int i = 0; (i < 50) && (file.hasNextLine()); i++){
             String readLine = file.nextLine();
             if(readLine.equals("KONSTYTUCJA")) return StatuteType.Constitution;
@@ -74,6 +89,11 @@ public class Parser{
         }
         return null;
     }
+
+    public static StatuteType getFileType() {
+        return fileType;
+    }
+    public static Scanner getScanner(){ return scanner; }
 }
 
 enum StatuteType {
--- src/StatuteReader.java
+++ src/StatuteReader.java
@@ -4,6 +4,27 @@ import java.util.Scanner;
 public class StatuteReader {
 
     public static void main(String[] arguments){
-        Parser parser = new Parser(arguments);
+        boolean q = false;
+        Parser.parse(arguments);
+        System.out.println(Parser.getScanner().nextLine());
+
+        while(!q){
+            System.out.println("Type q to quit");
+            System.out.println("Type s to see section");
+            System.out.println("Type c to see chapter");
+            System.out.println("Type a to see article");
+            if(Parser.getFileType() == StatuteType.Constitution) System.out.println("Type p to see preamble");
+
+
+            String lastChar;
+            Scanner s1 = new Scanner(System.in);
+            lastChar = s1.next();
+            lastChar = lastChar.toLowerCase();
+            switch(lastChar){
+                case "q": case "quit":
+                    q = true;
+            }
+        }
+
     }
 }
